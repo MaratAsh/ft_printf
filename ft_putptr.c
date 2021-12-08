4924dcf04ce7a021e38ce34854998febecf89051
Altagracia Cierra <alcierra@va-b1.kzn.21-school.ru>
2021-12-08 15:36:25 +0300

    ft_putptr.c was changed

--- ft_putptr.c
+++ ft_putptr.c
@@ -6,7 +6,7 @@
 /*   By: alcierra <alcierra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/31 15:26:24 by alcierra          #+#    #+#             */
-/*   Updated: 2021/12/08 15:32:39 by alcierra         ###   ########.fr       */
+/*   Updated: 2021/12/08 15:35:23 by alcierra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,12 +28,12 @@ int	ft_putptr_upper_fd(unsigned long long num, int fd)
 	return (res + ft_putnbr_base_usign_fd(num, "0123456789ABCDEF", fd));
 }
 
-int	ft_putptr_upper(unsigned long long num)
+int	ft_putptr_upper(unsigned long long num, int fd)
 {
-	return (ft_putptr_upper_fd(num, 1));
+	return (ft_putptr_upper_fd(num, fd));
 }
 
-int	ft_putptr_lower(unsigned long long num)
+int	ft_putptr_lower(unsigned long long num, int fd)
 {
-	return (ft_putptr_lower_fd(num, 1));
+	return (ft_putptr_lower_fd(num, fd));
 }
