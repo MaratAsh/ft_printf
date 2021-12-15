ddeeaf8167f152fdf1f4a0be510b5b13d5740dbc
Altagracia Cierra <alcierra@ev-p6.kzn.21-school.ru>
2021-12-15 16:16:04 +0300

    was changed with norm error

--- ft_printf.h
+++ ft_printf.h
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   printf.h                                           :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alcierra <alcierra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/31 16:45:45 by alcierra          #+#    #+#             */
-/*   Updated: 2021/12/04 17:10:59 by alcierra         ###   ########.fr       */
+/*   Updated: 2021/12/15 16:15:24 by alcierra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-int	ft_printf(const char *, ...);
+int	ft_printf(const char *format, ...);
 
 #endif
